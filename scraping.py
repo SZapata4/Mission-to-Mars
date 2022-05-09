79dfdc7ba033c2c26b504edac36acd44ffe036f2
Stephen Zapata Jr <stephenzapatajr@Stephens-Air.attlocal.net>
2022-05-08 21:08:50 -0500

    updating scraping.py file.

--- scraping.py
+++ scraping.py
@@ -1,396 +1,108 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 15,
-   "id": "a6935236",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Import Splinter and BeautifulSoup\n",
-    "from splinter import Browser\n",
-    "from bs4 import BeautifulSoup as soup\n",
-    "from webdriver_manager.chrome import ChromeDriverManager\n",
-    "import pandas as pd"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "cc768e97",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "\n",
-      "\n",
-      "====== WebDriver manager ======\n",
-      "Current google-chrome version is 100.0.4896\n",
-      "Get LATEST chromedriver version for 100.0.4896 google-chrome\n",
-      "Driver [/Users/stephenzapatajr/.wdm/drivers/chromedriver/mac64/100.0.4896.60/chromedriver] found in cache\n"
-     ]
+# Import Splinter, BeautifulSoup, and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
     }
-   ],
-   "source": [
-    "executable_path = {'executable_path': ChromeDriverManager().install()}\n",
-    "browser = Browser('chrome', **executable_path, headless=False)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "d2e6422a",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "True"
-      ]
-     },
-     "execution_count": 3,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# Visit the mars nasa news site\n",
-    "url = 'https://redplanetscience.com'\n",
-    "browser.visit(url)\n",
-    "# Optional delay for loading the page\n",
-    "browser.is_element_present_by_css('div.list_text', wait_time=1)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "id": "10c162fe",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "html = browser.html\n",
-    "news_soup = soup(html, 'html.parser')\n",
-    "slide_elem = news_soup.select_one('div.list_text')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "id": "5c9a08c1",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "<div class=\"content_title\">New Selfie Shows Curiosity, the Mars Chemist</div>"
-      ]
-     },
-     "execution_count": 5,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "slide_elem.find('div', class_='content_title')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "id": "6edef011",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'New Selfie Shows Curiosity, the Mars Chemist'"
-      ]
-     },
-     "execution_count": 6,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# Use the parent element to find the first `a` tag and save it as `news_title`\n",
-    "news_title = slide_elem.find('div', class_='content_title').get_text()\n",
-    "news_title"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "id": "d71b59be",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'The NASA rover performed a special chemistry experiment at the location captured in its newest self-portrait.'"
-      ]
-     },
-     "execution_count": 8,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# Use the parent element to find the paragraph text\n",
-    "news_p = slide_elem.find('div', class_='article_teaser_body').get_text()\n",
-    "news_p"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "id": "e804b268",
-   "metadata": {},
-   "source": [
-    "### Featured Images"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "id": "f72e0c73",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Visit URL\n",
-    "url = 'https://spaceimages-mars.com'\n",
-    "browser.visit(url)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 10,
-   "id": "67ea3ecf",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Find and click the full image button\n",
-    "full_image_elem = browser.find_by_tag('button')[1]\n",
-    "full_image_elem.click()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "id": "60875251",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Parse the resulting html with soup\n",
-    "html = browser.html\n",
-    "img_soup = soup(html, 'html.parser')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "id": "1e42b460",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'image/featured/mars2.jpg'"
-      ]
-     },
-     "execution_count": 13,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# Find the relative image url\n",
-    "img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')\n",
-    "img_url_rel"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 14,
-   "id": "ac569380",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'https://spaceimages-mars.com/image/featured/mars2.jpg'"
-      ]
-     },
-     "execution_count": 14,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# Use the base URL to create an absolute URL\n",
-    "img_url = f'https://spaceimages-mars.com/{img_url_rel}'\n",
-    "img_url"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "id": "a10f5221",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/html": [
-       "<div>\n",
-       "<style scoped>\n",
-       "    .dataframe tbody tr th:only-of-type {\n",
-       "        vertical-align: middle;\n",
-       "    }\n",
-       "\n",
-       "    .dataframe tbody tr th {\n",
-       "        vertical-align: top;\n",
-       "    }\n",
-       "\n",
-       "    .dataframe thead th {\n",
-       "        text-align: right;\n",
-       "    }\n",
-       "</style>\n",
-       "<table border=\"1\" class=\"dataframe\">\n",
-       "  <thead>\n",
-       "    <tr style=\"text-align: right;\">\n",
-       "      <th></th>\n",
-       "      <th>Mars</th>\n",
-       "      <th>Earth</th>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>description</th>\n",
-       "      <th></th>\n",
-       "      <th></th>\n",
-       "    </tr>\n",
-       "  </thead>\n",
-       "  <tbody>\n",
-       "    <tr>\n",
-       "      <th>Mars - Earth Comparison</th>\n",
-       "      <td>Mars</td>\n",
-       "      <td>Earth</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Diameter:</th>\n",
-       "      <td>6,779 km</td>\n",
-       "      <td>12,742 km</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Mass:</th>\n",
-       "      <td>6.39 × 10^23 kg</td>\n",
-       "      <td>5.97 × 10^24 kg</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Moons:</th>\n",
-       "      <td>2</td>\n",
-       "      <td>1</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Distance from Sun:</th>\n",
-       "      <td>227,943,824 km</td>\n",
-       "      <td>149,598,262 km</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Length of Year:</th>\n",
-       "      <td>687 Earth days</td>\n",
-       "      <td>365.24 days</td>\n",
-       "    </tr>\n",
-       "    <tr>\n",
-       "      <th>Temperature:</th>\n",
-       "      <td>-87 to -5 °C</td>\n",
-       "      <td>-88 to 58°C</td>\n",
-       "    </tr>\n",
-       "  </tbody>\n",
-       "</table>\n",
-       "</div>"
-      ],
-      "text/plain": [
-       "                                    Mars            Earth\n",
-       "description                                              \n",
-       "Mars - Earth Comparison             Mars            Earth\n",
-       "Diameter:                       6,779 km        12,742 km\n",
-       "Mass:                    6.39 × 10^23 kg  5.97 × 10^24 kg\n",
-       "Moons:                                 2                1\n",
-       "Distance from Sun:        227,943,824 km   149,598,262 km\n",
-       "Length of Year:           687 Earth days      365.24 days\n",
-       "Temperature:                -87 to -5 °C      -88 to 58°C"
-      ]
-     },
-     "execution_count": 16,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "df = pd.read_html('https://galaxyfacts-mars.com')[0]\n",
-    "df.columns=['description', 'Mars', 'Earth']\n",
-    "df.set_index('description', inplace=True)\n",
-    "df"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 17,
-   "id": "a6b8782b",
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'<table border=\"1\" class=\"dataframe\">\\n  <thead>\\n    <tr style=\"text-align: right;\">\\n      <th></th>\\n      <th>Mars</th>\\n      <th>Earth</th>\\n    </tr>\\n    <tr>\\n      <th>description</th>\\n      <th></th>\\n      <th></th>\\n    </tr>\\n  </thead>\\n  <tbody>\\n    <tr>\\n      <th>Mars - Earth Comparison</th>\\n      <td>Mars</td>\\n      <td>Earth</td>\\n    </tr>\\n    <tr>\\n      <th>Diameter:</th>\\n      <td>6,779 km</td>\\n      <td>12,742 km</td>\\n    </tr>\\n    <tr>\\n      <th>Mass:</th>\\n      <td>6.39 × 10^23 kg</td>\\n      <td>5.97 × 10^24 kg</td>\\n    </tr>\\n    <tr>\\n      <th>Moons:</th>\\n      <td>2</td>\\n      <td>1</td>\\n    </tr>\\n    <tr>\\n      <th>Distance from Sun:</th>\\n      <td>227,943,824 km</td>\\n      <td>149,598,262 km</td>\\n    </tr>\\n    <tr>\\n      <th>Length of Year:</th>\\n      <td>687 Earth days</td>\\n      <td>365.24 days</td>\\n    </tr>\\n    <tr>\\n      <th>Temperature:</th>\\n      <td>-87 to -5 °C</td>\\n      <td>-88 to 58°C</td>\\n    </tr>\\n  </tbody>\\n</table>'"
-      ]
-     },
-     "execution_count": 17,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "df.to_html()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 18,
-   "id": "347e5968",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "browser.quit() "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "b5c4b4ee",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "PythonData",
-   "language": "python",
-   "name": "pythondata"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
+
+
+def mars_news(browser):
+
+    # Scrape Mars News
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
+
+
+
+# ## JPL Space Images Featured Image
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    return img_url
+
+# ## Mars Facts
+
+def mars_facts():
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
+    except BaseException:
+        return None
+
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
